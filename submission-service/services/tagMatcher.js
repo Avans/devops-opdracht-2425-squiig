d2fdd5843f9dc75c48e4c53a33d64d259c781b00
Stan <8138029+squiig@users.noreply.github.com>
2025-04-11 16:24:21 +0200

    Attempt to fix tagMatcher error

--- submission-service/services/tagMatcher.js
+++ submission-service/services/tagMatcher.js
@@ -1,71 +1,77 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function compareImage(target, submissionImage) {
-    try {
-        if (target.imageData === submissionImage) {
-            throw {message:`Target image cannot be exactly the same as entry image.`, code: 400};
-        }
-        const formData = new FormData();
-        formData.append('image_base64', submissionImage);
-
-        let entryConfig = {
-            method: 'post',
-            url: `${process.env.IMAGGA_ENDPOINT}/v2/tags`,
-            headers: {
-                'Authorization': 'Basic ' + process.env.IMAGGA_AUTH
-            },
-            data: formData
-        };
-
-        const formData2 = new FormData();
-        formData2.append('image_base64', target.imageData);
-
-
-        let targetConfig = {
-            method: 'post',
-            url: `${process.env.IMAGGA_ENDPOINT}/v2/tags`,
-            headers: {
-                'Authorization': 'Basic ' + process.env.IMAGGA_AUTH
-            },
-            data: formData2
-        };
-
-        try {
-            const [firstResponse, secondResponse] = await Promise.all([
-                axios.request(targetConfig),
-                axios.request(entryConfig)
-              ]);
-              
-            let score = compareTags(firstResponse.data.result.tags, secondResponse.data.result.tags);
-            return score;
-        } catch (error) {
-            console.log("error with imagga call: " + error)
-            throw {message:"GetTargetWithTags went wrong!", code: 424};
-        }
-   
-
-
-        
-    } catch (message) {
-        console.log("error in tagMatcher: " + error)
-        throw {message: error.message, code: error.code||500};
+  try {
+    if (target.imageData === submissionImage) {
+      throw {
+        message: `Target image cannot be exactly the same as entry image.`,
+        code: 400,
+      };
     }
-}
+    const formData = new FormData();
+    formData.append("image_base64", submissionImage);
 
+    let entryConfig = {
+      method: "post",
+      url: `${process.env.IMAGGA_ENDPOINT}/v2/tags`,
+      headers: {
+        Authorization: "Basic " + process.env.IMAGGA_AUTH,
+      },
+      data: formData,
+    };
+
+    const formData2 = new FormData();
+    formData2.append("image_base64", target.imageData);
+
+    let targetConfig = {
+      method: "post",
+      url: `${process.env.IMAGGA_ENDPOINT}/v2/tags`,
+      headers: {
+        Authorization: "Basic " + process.env.IMAGGA_AUTH,
+      },
+      data: formData2,
+    };
+
+    try {
+      const [firstResponse, secondResponse] = await Promise.all([
+        axios.request(targetConfig),
+        axios.request(entryConfig),
+      ]);
+
+      let score = compareTags(
+        firstResponse.data.result.tags,
+        secondResponse.data.result.tags
+      );
+      return score;
+    } catch (error) {
+      console.log("error with imagga call: " + error);
+      throw { message: "GetTargetWithTags went wrong!", code: 424 };
+    }
+  } catch (error) {
+    console.log("error in tagMatcher: " + error);
+    throw { message: error.message, code: error.code || 500 };
+  }
+}
 
 function compareTags(targetTags, submissionTags) {
-    let matchingConfidence = 0;
-    for (const targetTag of targetTags) {
-        const matchingSubmissionTag = submissionTags.find((tag) => tag.tag.en === targetTag.tag.en);
-        if (matchingSubmissionTag) {
-            matchingConfidence += matchingSubmissionTag.confidence;
-        }
+  let matchingConfidence = 0;
+  for (const targetTag of targetTags) {
+    const matchingSubmissionTag = submissionTags.find(
+      (tag) => tag.tag.en === targetTag.tag.en
+    );
+    if (matchingSubmissionTag) {
+      matchingConfidence += matchingSubmissionTag.confidence;
     }
-    const totalConfidenceAllTags = targetTags.reduce((accumulator, tag) => accumulator + tag.confidence, 0);
-    const percentage = Math.round((100 * matchingConfidence) / totalConfidenceAllTags);
-    console.log(`${percentage}% of matching confidence`);
-    return percentage;
+  }
+  const totalConfidenceAllTags = targetTags.reduce(
+    (accumulator, tag) => accumulator + tag.confidence,
+    0
+  );
+  const percentage = Math.round(
+    (100 * matchingConfidence) / totalConfidenceAllTags
+  );
+  console.log(`${percentage}% of matching confidence`);
+  return percentage;
 }
 
-
-module.exports.compareImage = compareImage
+module.exports.compareImage = compareImage;
