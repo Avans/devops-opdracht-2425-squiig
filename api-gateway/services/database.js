2a5fdeb974895076fb1cafad6c1e9ba144c3cbd9
Stan <8138029+squiig@users.noreply.github.com>
2025-06-21 23:17:00 +0200

    Add ensureConnection function in db service

--- api-gateway/services/database.js
+++ api-gateway/services/database.js
@@ -2,11 +2,31 @@ import mongoose from "mongoose";
 import seeder from "../seed.js";
 const mongoUri = process.env.MONGO_URL || "mongodb://gatewaydb:27017/users";
 
+async function ensureConnected() {
+  if (mongoose.connection.readyState === 1) return; // Connected
+  if (mongoose.connection.readyState === 2) { // Connecting
+    // Wait until it's connected
+    try {
+      await new Promise((resolve, reject) => {
+        mongoose.connection.once('connected', resolve);
+        mongoose.connection.once('error', reject);
+      });
+      return; // Connected
+    } catch (err) {
+      console.error('Failed to connect to database:', err);
+    }
+  }
+  // Disconnected or disconnecting, so reconnect
+  return await connect();
+}
+
+
 async function connect() {
   try {
     console.log("Connecting to MongoDB with Mongoose...");
-    await mongoose.connect(mongoUri);
+    const m = await mongoose.connect(mongoUri);
     console.log("✅ MongoDB connected for DB service");
+    return m;
   } catch (connErr) {
     console.error("❌ Error connecting to MongoDB:", connErr);
   }
@@ -23,11 +43,12 @@ async function disconnect() {
 }
 
 async function connectAndSeed() {
-  await connect();
+  await ensureConnected();
   await seeder();
 }
 
 export default {
+  ensureConnected,
   connect,
   disconnect,
   connectAndSeed,
