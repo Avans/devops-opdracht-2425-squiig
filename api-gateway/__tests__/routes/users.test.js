7fc3ee069d392c7c3017781e3d9ad1c94c893a52
Stan <8138029+squiig@users.noreply.github.com>
2026-04-10 15:43:47 +0200

    Fix users test by authenticating with a JWT admin token

--- api-gateway/__tests__/routes/users.test.js
+++ api-gateway/__tests__/routes/users.test.js
@@ -1,5 +1,6 @@
 import {jest} from '@jest/globals';
 import request from "supertest";
+import jwt from "jsonwebtoken";
 import app from "../../app.js";
 import db from "../../services/database.js";
 import User from "../../models/user.js";
@@ -22,14 +23,17 @@ describe("Get Users", () => {
 
   it("should get all users in array", async () => {
     console.log("Starting test");
+    const adminUser = await User.create({ email: "admin@test.com", passwordHash: "adminpass", role: "admin" });
+    const token = jwt.sign({ userId: adminUser._id }, process.env.PRIVATE_KEY || "SECRET");
+
     const expected = { email: "foo@bar.com", passwordHash: "foobar", role: "user" };
     await User.create(expected);
     delete expected._id;
 
-    const res = await request(app).get("/read/users");
+    const res = await request(app).get("/read/users").set("Authorization", `Bearer ${token}`);
     expect(res.statusCode).toEqual(200);
-    expect(res.body.length).toEqual(1);
-    expect(res.body[0]).toEqual(expect.objectContaining(expected));
+    expect(res.body.length).toEqual(2);
+    expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining(expected)]));
     console.log("Test done");
   });
 });
